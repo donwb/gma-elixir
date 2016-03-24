6445a739aef042ee60a46fb090ff8a62483cf0bb
donwb <don.browning@gmail.com>
2016-03-24 04:54:10 +0000

    added seed file for db

---
+++ db/seed.sql
@@ -0,0 +1,21 @@
+insert into homes (city, image, display_order) values ('Cumming', 'chad-new.jpg', 0);
+insert into homes (city, image, display_order) values ('Dunwoody', 'DSC_2800.jpg', 1);
+insert into homes (city, image, display_order) values ('Marietta', 'DSC_2289.jpg', 2);
+insert into homes (city, image, display_order) values ('Marietta', 'house.jpg', 3);
+insert into homes (city, image, display_order) values ('Marietta', 'DSC_2100.jpg', 4);
+insert into homes (city, image, display_order) values ('Kennesaw', 'DSC_2097.jpg', 5);
+insert into homes (city, image, display_order) values ('Marietta', 'DSC_2104.jpg', 6);
+insert into homes (city, image, display_order) values ('Kennesaw', 'DSC_2089.jpg', 7);
+insert into homes (city, image, display_order) values ('Powder Springs', 'DSC_2284.jpg', 8);
+insert into homes (city, image, display_order) values ('Dallas', 'DSC_2056.jpg', 9);
+insert into homes (city, image, display_order) values ('Marietta', 'IMG_4844.jpg', 10);
+insert into homes (city, image, display_order) values ('Hiram', 'DSC_2076.jpg', 11);
+insert into homes (city, image, display_order) values ('Hiram', 'DSC_2057.jpg', 12);
+insert into homes (city, image, display_order) values ('Fulton', 'IMG_2430.jpg', 13);
+insert into homes (city, image, display_order) values ('Dallas', 'DSC_8650.jpg', 14);
+insert into homes (city, image, display_order) values ('Dekalb', 'DSC_8816.jpg', 15);
+insert into homes (city, image, display_order) values ('Atlanta', 'DSC_2035.jpg', 16);
+insert into homes (city, image, display_order) values ('Marietta', 'marietta.jpg', 17);
+insert into homes (city, image, display_order) values ('Cumming', 'chad-old.jpg', 18);
+
+
