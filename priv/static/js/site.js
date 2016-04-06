9722ea552425719f877cd23914ce41d0138b92df
donwb <don.browning@gmail.com>
2016-04-06 20:04:50 +0000

    converted to handlebars template

--- priv/static/js/site.js
+++ priv/static/js/site.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
+  
   $.getJSON('/data', function(json){
     var headerSource = $("#header-template").html();
     var footerSource = $('#footer-template').html();
+    var homesSource = $('#homes-template').html();
 
     var headerTemplate = Handlebars.compile(headerSource);
     var footerTemplate = Handlebars.compile(footerSource);
@@ -9,5 +11,31 @@ $(document).ready(function(){
     $('#header').html(headerTemplate(json));
     $('#footer').html(footerTemplate(json));
 
+    $.getJSON('/homes', function(h){
+      var homes = {homes:h}
+      
+      var homesTemplate = Handlebars.compile(homesSource);
+      $('#homes').html(homesTemplate(homes));
+    })
   });
+  
+  
 });
+
+Handlebars.registerHelper('grouped_each', function(every, context, options){
+  var out = "", subcontext = [], i;
+  if (context && context.length > 0) {
+    
+      for (i = 0; i < context.length; i++) {
+          if (i > 0 && i % every === 0) {
+              out += options.fn(subcontext);
+              subcontext = [];
+          }
+          subcontext.push(context[i]);
+      }
+      
+      out += options.fn(subcontext);
+  }
+  
+  return out;
+})
